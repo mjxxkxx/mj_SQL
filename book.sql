0b7e281c3db2e0ae27e3df7a0350dd5dfaeef37f
MJ <mjinkim17@gmail.com>
2024-07-25 11:26:29 +0900

    Update book.sql

--- book.sql
+++ book.sql
@@ -1,11 +1,11 @@
 -- create database BookDB;
 use BookDB;
-/* create table TestTable(
+/* create table Testtable(
 	col_1 int,
 -- -- col_2 varchar(20),
 -- -- col_3 datetime
 );-*/
--- drop table TestTable;
+-- drop table Testtable;
 
 /* create table Book(
 	bookid int,
@@ -175,7 +175,7 @@ select distinct publisher from book;
 -- 책의 가격을 23,000원으로 수정
 update Book set price = 23000 where bookid = 101;
 
--- VARCHAR(10) 자료형을 가진 isbn 속성 추가
+-- varchar(10) 자료형을 가진 isbn 속성 추가
 alter table Book add isbn varchar(10);
 select * from book;
 
@@ -183,10 +183,12 @@ select * from book;
 alter table book modify isbn int;
 
 -- Book 테이블의 isbn 속성의 이름을 '일련번호'로 변경
-alter table book change isbn 일련번호 int;
+alter table book change isbn 일련번호;
 
 -- Book 테이블의 일련번호 속성을 삭제
 alter table book drop column 일련번호;
+
+-- 기본키(primary key)
 
 -- newBook 테이블 생성하기 bookid를 기본키로 지정
 create table newBook(
@@ -208,36 +210,38 @@ values(100, '프로그래밍', '한빛아카데미', 30000); 기본키로 저장
 insert into newBook(bookid, bookname, publisher, price)
 values(null, '데이터 시각화', '생능출판사', 27000);*/
 
-
 select * from newBook;
 
 -- 실습 NOT NULL
 
 -- 테이블 속성 변경하기 : bookname과 publisher에 null을 들어가지 못하도록 하는 함수!
 
--- ALTER 함수는 계속적으로 저장되는 함수임
+-- alter 함수는 계속적으로 저장되는 함수임
 
-ALTER TABLE newBook MODIFY bookname VARCHAR(20) NOT NULL;
+alter table newBook modify bookname varchar(20) NOT NULL;
 
-ALTER TABLE newBook MODIFY publisher VARCHAR(20) NOT NULL;
+alter table newBook modify publisher varchar(20) NOT NULL;
 
-ALTER TABLE newBook MODIFY publisher VARCHAR(20) NULL;
+alter table newBook modify publisher varchar(20) NULL;
 -- 이후 이전에 실행가능했던 아래의 쿼리를 실행할 수 없음 : 
 -- 설정하기전 디폴트 값은 null 허용
-
+/*
 insert into newBook(bookid, bookname, publisher, price) 
-values(101,null,null,25000);
+values(101, null, null, 25000);
 -- 위의 bookname, publisher에 null이 들어가있는 이유 : 
 -- bookid는 primary key를 지정해줬지만, name과 publisher는 키를 입력하지 않았기때문에 null이 들어갈 수 있음 
 
 insert into newBook(bookid, bookname, publisher, price) 
-values(102,'데이터시각화','생능출판사',25000);
+values(102, '데이터시각화', '생능출판사', 25000);
+*/
 
 select * from newBook;
 
-alter TABLE newBook MODIFY price INT default 10000;
+-- 테이블 속성 변경하기 : price 속성의 기본값을 10,000원으로 지정
+alter table newBook modify price INT default 10000;
 
 -- 기본 값 확인
+/*
 insert into newBook(bookid, bookname, publisher) 
 values (103, '프로그래밍','한빛아카데미');
 select * from newBook;
@@ -246,17 +250,17 @@ select * from newBook;
 insert into newBook(bookid, bookname, publisher,price) 
 values (104, '프로그래밍','한빛아카테미',null);
 select * from newBook;
+*/
 
--- 외래키 : 다른 릴레이션(테이블)의 기본키를 참조하는 속성
 -- 기존 데이터 모두 제거 후 입력
 
 delete from newBook;
 
 -- 아래의 항목 추가해 두기 : ppt 2.SQL 111p
-
-INSERT INTO newBook (bookid, bookname, publisher, price) 
+/*
+insert into newBook (bookid, bookname, publisher, price) 
 values (100, '데이터베이스', '한빛아카데미', 27000);
-INSERT INTO newBook (bookid, bookname, publisher, price) 
+insert into newBook (bookid, bookname, publisher, price) 
 values (101, '파이썬', '한빛아카데미', 22000);
 insert into newBook (bookid, bookname, publisher, price) 
 values (102, 'JSP 프로그래밍', '생능출판사', 26000);
@@ -276,36 +280,45 @@ insert into newBook (bookid, bookname, publisher, price)
 values (109, '홈페이지만들기', '한빛아카데미', 32000);
 insert into newBook (bookid, bookname, publisher, price) 
 values (110, '데이터시각화', '생능출판사', 27000);
+*/
+
+-- 외래키 (Foreign Key) : 다른 릴레이션(테이블)의 기본키를 참조하는 속성 - 연결고리를 만들어 놓는 것
+-- create table 테이블 이름(
+--         속성이름1 데이터타입 primary key,
+--         속성이름2 데이터타입,
+--         속성이름3 데이터타입,
+--         foreign key(속성이름2) references 참초할테이블이름(속성이름)
+-- );
 
 -- newOrders 테이블 생성 /ppt 2.SQL 114p
-
-CREATE TABLE newOrders(
-	orderid    VARCHAR(10)    primary key,
-	bookid    INT    NOT NULL,
-	member    VARCHAR(10)    NOT NULL,
-	address    VARCHAR(20)    NOT NULL,
+/*
+create table newOrders(
+	orderid varchar(10)    primary key,
+	bookid int NOT NULL,
+	member varchar(10) NOT NULL,
+	address varchar(20) NOT NULL,
 	foreign key(bookid) references newBook(bookid)
 );
 
 select * from newOrders;
-/* newOrders 테이블에 데이터 입력 */
+
+-- newOrders 테이블에 데이터 입력 
 insert into newOrders(orderid, bookid, member, address) 
 values('p001',102,'정수아','서울');
 
-/* newOrders 테이블에 2번째 데이터 입력 */
+-- newOrders 테이블에 2번째 데이터 입력 
 -- 오류의 이유 : bookid(여기서 120)가 참조테이블에 존재하지 않음
 insert into newOrders(orderid, bookid, member, address) 
 values('p002',120,'정수아','서울');
 
 -- newBook(참조테이블)의 데이터 삭제하기
-delete from newBook where bookid=102;
+delete from newBook where bookid=102; */
 
-/* 참조 테이블의 데이터까지 한번에 삭제하고 싶다면 외래키를 지정할때,
- on delete cascade옵션을 추가하면 됨
-foreign key(속성이름)
-references 참조할테이블 이름(속성이름)on delete cascade
+/* 참조 테이블의 데이터까지 한번에 삭제하고 싶다면 외래키를 지정할때, on delete cascade옵션을 추가하면 됨
+foreign key(속성이름) references 참조할테이블 이름(속성이름)on delete cascade
 */
 
+/*
 -- newOrders 삭제 후 다시 입력 --
 delete from newOrders;
 
@@ -329,24 +342,117 @@ insert into newOrders(orderid, bookid, member, address)
 values ('p009', 107, '오한솔', '경기');
 insert into newOrders(orderid, bookid, member, address) 
 values ('p010', 103, '김현우', '서울');
- 
+*/
  /*
-두 테이블 연결하여 orderTable에서 책에 대한 정보 검색 : 조인 
+두 테이블 연결하여 ordertable에서 책에 대한 정보 검색 : 조인 
+
 조인 : SELECT 테이블이름.열이름 FROM 참조할 테이블이름1, 테이블이름2 
 WHERE 조건 AND 테이블1.b = 테이블2.b(테이블1과 테이블2의 어떠한 열은 같다!)
 3개 이상의 테이블 사용도 가능, 테이블이름.열이름에서 테이블이름은 필수가 아니긴하지만 이해 및 가독성을 위해 작성해줌
 (두 테이블의 열이름이 같은 경우는 반드시 작성해야함)
-SELECT A.a,B.d FROM A, B WHERE 조건 AND A.b = B.b 
-SELECT a,d FROM A, B WHERE A.b = B.b */
+
+select A.a,B.d from A, B 
+where 조건 and A.b = B.b;
+
+select a,d from A, B 
+where A.b = B.b */
 
 -- 김현우가 주문한 책 이름과 가격
-select newBook.bookname, newBook.price FROM newBook, newOrders 
-WHERE newOrders.member = "김현우" and newBook.bookid = newOrders.bookid;
+select newBook.bookname, newBook.price 
+from newBook, newOrders 
+where newOrders.member = '김현우' and newBook.bookid = newOrders.bookid;
 
--- 실습2 : 운송장 번호 출력
+-- 실습2 : 2개의 테이블을 이용한 조인 검색 (운송장 번호 출력)
+select * from newBook;
+select * from newOrders;
 
-select *from newBook;
-select *from newOrders;
+select newOrders.orderid, newOrders.member, newBook.bookname, newBook.price, newOrders.address 
+from newBook, newOrders 
+where newOrders.bookid = newBook.bookid;
 
-select newOrders.orderid, newOrders.member,newBook.bookname,newBook.price,newOrders.address 
-FROM newBook, newOrders WHERE newOrders.bookid = newBook.bookid;
+-- 문자열 연결하기 : concat
+select concat('data', 'base');
+
+-- bookname과 publisher 연결하기
+select concat(bookname, ' : ', publisher) from Book;
+
+-- 대소문자 변경하기 : lower(소문자), upper(대문자)
+select lower('Database SQL'), upper('Database SQL');
+
+-- 영어 문자열 길이 반환하기 : length
+select length('Databsae SQL');
+
+-- 한글 문자열 길이 반환하기 : 한글 한글자가 3byte
+select length('데이터베이스');
+
+-- 정확한 문자열 길이 반환하기 : char_length(자릿수)
+select char_length('데이터베이스');
+
+-- 문자열 왼쪽 공백 제거하기 : ltrim
+select ltrim('       데이터베이스       ');
+
+-- 문자열 오른쪽 공백 제거하기 : rtrim
+select rtrim('       데이터베이스       ');
+
+-- 문자열 양쪽 공백 제거하기 : trim
+select trim('       데이터베이스       ');
+
+-- 문자열 변경하기 : replace(열 또는 문자, 변경하려는 문자, 변경문자)
+select replace('데이터베이스 프로그래밍', '데이터베이스', '웹');
+
+-- DBMS 시스템상의 오늘 날짜를 반환 : sysdate / now / current_date / current_time
+select sysdate();
+select now();
+select current_date();
+select current_time();
+select month('2024-07-25 10:28:02');
+select day('2024-07-25 10:28:02');
+select hour('2024-07-25 10:28:02');
+
+-- 날짜 차이 계산하기 : datediff(기준날짜, 비교날짜)
+select datediff('2024-10-12', '2024-07-25');
+
+-- 날짜 더하기 : adddate(기준날짜, 숫자)
+select adddate('2024-10-12', 10);
+
+-- 절댓값 계산하기 : abs(숫자)
+select abs(-80), abs(80);
+
+-- 반올림한 값을 구하기 : round(숫자, 자릿수)
+select round(12.678, 1);
+
+-- 6-1 도서 제목에 '데이터'가 포함된 도서를 'Data'로 변경한 후 도서 목록을 출력
+select replace(bookname, '데이터', 'Data')
+bookname, publisher, price from Book;
+
+-- 6-2 한빛아카데미에서 출판한 도서의 제목과 제목의 문자 수, 바이트 수를 출력
+select bookname as '제목', 
+char_length(bookname)as '문자수', 
+length(bookname) as '바이트수' from Book 
+where publisher = '한빛아카데미';
+
+-- 6-3  Book 테이블에 새로운 속성 추가주문 날짜 : orderdate(DATE타입)
+alter table Book 
+add orderdate date;
+select * from Book;
+
+-- 6-4 각 도서에 주문 날짜 데이터 추가 후, 목록을 출력
+update book set orderdate = '2024-05-12' where bookid = 100;
+update book set orderdate = '2024-05-04' where bookid = 101;
+update book set orderdate = '2024-06-02' where bookid = 102;
+update book set orderdate = '2024-06-10' where bookid = 103;
+update book set orderdate = '2024-05-12' where bookid = 104;
+update book set orderdate = '2024-04-12' where bookid = 105;
+update book set orderdate = '2024-03-15' where bookid = 106;
+update book set orderdate = '2024-05-12' where bookid = 107;
+update book set orderdate = '2024-06-07' where bookid = 108;
+update book set orderdate = '2024-04-26' where bookid = 109;
+update book set orderdate = '2024-05-28' where bookid = 110;
+
+-- 6-5 주문일로부터 10일 후 매출을 확정한다고 할 때, 각 주문의 확정 일자를 계산
+select bookid as '주문번호', orderdate as '주문일자',
+adddate(orderdate, 10) as '확정일자'
+from book;
+
+
+
